0af097942c324bbcc56e51acacba4d312cd2929c
jcazorlap <jucapo2006@gmail.com>
2025-03-28 12:18:32 +0100

    Actualizado

--- src/main/java/org/iesalandalus/programacion/tallermecanico/vista/texto/VistaTexto.java
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/vista/texto/VistaTexto.java
@@ -13,93 +13,113 @@ import java.util.List;
 
 import static org.iesalandalus.programacion.tallermecanico.vista.texto.Consola.*;
 
-public class VistaTexto {
-    public GestorEventos gestorEventos;
+public class VistaTexto implements org.iesalandalus.programacion.tallermecanico.vista.Vista {
+    @Override
+    public GestorEventos gestorEventos() {
 
+    }
+
+    @Override
     public void comenzar() {
 
     }
 
-    private void ejecutar(Evento opcion) {
-
-    }
-
+    @Override
     public void terminar() {
 
     }
 
+    @Override
     public Cliente leerCliente() {
 
     }
 
+    @Override
     public Cliente leerClienteDni() {
 
     }
 
+    @Override
     public String leerNuevoNombre() {
 
     }
 
+    @Override
     public String leerNuevoTelefono() {
 
     }
 
+    @Override
     public Vehiculo leerVehiculo() {
 
     }
 
+    @Override
     public Vehiculo leerVehiculoMatricula() {
 
     }
 
+    @Override
     public Trabajo leerRevision() {
 
     }
 
+    @Override
     public Trabajo leerMecanico() {
 
     }
 
+    @Override
     public Trabajo leerTrabajoVehiculo() {
 
     }
 
+    @Override
     public int leerHoras() {
 
     }
 
+    @Override
     public float leerPrecioMaterial() {
 
     }
 
+    @Override
     public LocalDate leerFechaCierre() {
 
     }
 
+    @Override
     public void notificarResultado(Evento evento, String texto, boolean exito) {
 
     }
 
+    @Override
     public void mostrarCliente(Cliente cliente) {
 
     }
 
+    @Override
     public void mostrarVehiculo(Vehiculo vehiculo) {
 
     }
 
+    @Override
     public void mostrarTrabajo(Trabajo trabajo) {
 
     }
 
+    @Override
     public void mostrarClientes(List<Cliente> clientes) {
 
     }
 
+    @Override
     public void mostrarVehiculos(List<Vehiculo> vehiculos) {
 
     }
 
+    @Override
     public void mostrarTrabajos(List<Trabajo> trabajos) {
 
     }
